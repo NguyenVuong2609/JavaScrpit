cc6939fff93d71b65c1ccb40d676b311d3a39e8e
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2022-11-09 10:36:00 +0700

    update911

---
+++ JS/2.2/demo.js
@@ -0,0 +1,4 @@
+document.write("hello");
+document.getElementById("capital1").innerHTML = "New York";
+document.getElementsByClassName("capital")[0].innerHTML = "Kuala Lumpur";
+document.getElementById("capital1").style.color = "red";

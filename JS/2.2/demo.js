a9064ea5cd8374d76c32f42e00261b6436cc6bdc
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2022-11-09 11:24:42 +0700

    update911

--- JS/2.2/demo.js
+++ JS/2.2/demo.js
@@ -1,4 +1,18 @@
 document.write("hello");
 document.getElementById("capital1").innerHTML = "New York";
-document.getElementsByClassName("capital")[0].innerHTML = "Kuala Lumpur";
+document.getElementsByClassName("capital")[0].innerHTML = "Kuala <br/> Lumpur";
 document.getElementById("capital1").style.color = "red";
+
+window.alert("Vui lòng trả tiền để xem Full nội dung \n0976106636");
+
+function myFunction() {
+    let text;
+    if (confirm("Nhận 100k") == true) {
+      text = "Không làm mà đòi có ăn thì...";
+    } else {
+      text = "Chúc may mắn lần sau!";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+
+  prompt("Enter your phone number:");

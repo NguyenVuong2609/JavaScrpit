bdebc7093ec3a7774d767c01db8de59d8cb7b46e
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2022-11-09 21:09:36 +0700

    update911

---
+++ JS/SS10-CB/BT4/bt4.js
@@ -0,0 +1,7 @@
+let inputWidth = prompt("Enter the width")
+let inputHeight = prompt("Enter the height")
+
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
+let area = width * height
+document.write("The area is: " + area)

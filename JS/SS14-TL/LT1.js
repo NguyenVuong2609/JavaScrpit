e6f8f8cbd370010baa99b5d9ed06a49eeb6bea92
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2022-11-21 10:59:32 +0700

    2111

---
+++ JS/SS14-TL/LT1.js
@@ -0,0 +1,47 @@
+// Bai 1
+// function alertMessage(){
+//     alert("Xin chao!");
+// }
+// alertMessage();
+
+// Bai 2
+// function totalNum(x) {
+//     x = x + 1;
+//     console.log(x);
+//     return x;
+// }
+// totalNum(x = parseInt(prompt('Xin moi nhap so: ')));
+// totalNum(7);
+// console.log(x);
+
+// Bai 3
+// function compareNum(a,b){
+//     if (a > b) {
+//         alert("A lon hon B.")
+//     } else {
+//         let c = a + b;
+//         console.log(c);
+//         return c;
+//     }
+// }
+
+// compareNum(a = parseInt(prompt("Xin moi nhap so A: ")), b = parseInt(prompt("Xin moi nhap so B: ")));
+
+// Bai 4
+// function addNumbers() {
+//     firstNum = 4;
+//     secondNum = 8;
+//     result = firstNum + secondNum;
+//     return result;
+// }
+//     result = 0;
+//     alert(result);
+//     result = addNumbers();
+//     alert(result);
+
+// Bai 5
+let star = ["Polaris","Aldebaran","Deneb","Vega","Altair","Dubhe","Regulus"];
+let constellation = ["Ursa Minor","Tarurus"]
+function findStar() {
+
+}

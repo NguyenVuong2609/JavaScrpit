e49bdafcc43a71405e3edbda86c45b7680e3ff3e
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2022-11-21 11:26:24 +0700

    2111

---
+++ JS/SS14-TL/LT2.js
@@ -0,0 +1,24 @@
+// Bai 1
+// function squareNum(){
+//     let x = parseFloat(prompt("Xin moi nhap so ban muon tinh binh phuong: "));
+//     x = x * x;
+//     return x;
+// }
+// document.getElementById("result").innerHTML = "Binh phuong cua so ban nhap la: " + squareNum();
+
+// Bai 2
+// function findCircleArea() {
+//     const Pi = 3.14;
+//     let R = parseFloat(prompt("Xin moi nhap ban kinh hinh tron cua ban: "));
+//     let S = R * R * Pi;
+//     return S;
+// }
+// document.getElementById("result").innerHTML = "Dien tich hinh tron cua ban la: " + findCircleArea();
+
+// function findCirclePerimeter() {
+//     const Pi = 3.14;
+//     let R = parseFloat(prompt("Xin moi nhap ban kinh hinh tron cua ban: "));
+//     let P = R * 2 * Pi;
+//     return P;
+// }
+// document.getElementById("result").innerHTML = "Chu vi hinh tron cua ban la: " + findCirclePerimeter();

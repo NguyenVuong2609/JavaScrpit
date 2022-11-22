8903d1fdbe6fb69a38c5987283b747665c1ee4bc
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2022-11-22 14:17:05 +0700

    2211

--- JS/SS14-TL/BT3.js
+++ JS/SS14-TL/BT3.js
@@ -11,12 +11,15 @@ function addProduct() {
 // Hien san pham 
 function displayProduct() {
     let display = "";
+    display += "<tr>";
+    display += "<th>" + "STT" + "</th>" + "<th>" + "Product's Name" + "</th>" + "<th>" + "Action" + "</th>";
     for (let i = 0; i < productList.length; i++) {
         display += "<tr>";
         display += "<td>" + (i + 1) + "</td>" + "<td>" + productList[i] + "</td>" + "<td>" + "<span>" + "<input type='button' onclick='editProduct("+i+")' id='editButton' value='Edit'></input>" + "</span>" + " " + "<span>" + "<input type='button' onclick='deleteProduct("+i+")' id='deleteButton' value='Delete'></input>" + "</span>" + "</td>";  
         display += "</tr>";
     }
     document.getElementById("displayProduct").innerHTML = display;
+    document.getElementById("total").innerHTML = "Tổng số lượng sản phẩm là: " + productList.length;
 }
 
 // Xoa san pham 
@@ -24,12 +27,11 @@ function deleteProduct(i) {
     productList.splice(i ,1)
     displayProduct();
 }
-
-var value ="";
+var value = "";
 // Hien ten san pham len thanh edit
 function editProduct(i) {
    document.getElementById("editName").value = productList[i];
-    value = i;
+   value =parseInt(i);
    displayProduct();
 }
 
@@ -38,3 +40,5 @@ function changeProduct() {
     productList[value] = document.getElementById("editName").value;
     displayProduct();
 }
+
+
